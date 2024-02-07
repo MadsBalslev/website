f0145897e546969e71b26798dbc6c20f830b504f
Mads Balslev <madspbalslev@gmail.com>
2024-02-07 21:32:41 +0100

    Base styling

---
+++ src/lib/components/Navbar/Navlink.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let to = "/"
+  export let active = false
+</script>
+
+<a href={to} class="{active ? 'active' : ''}">
+  <slot></slot>
+</a>
+
+<style>
+  a {
+    color: #CCC5B9;
+    text-decoration: none;
+  }
+
+  a:hover::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 3px;
+    background-color: #EB5E28;
+  }
+
+  a.active {
+    color: #fffcf2;
+  }
+
+  a.active::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 3px;
+    background-color: #EB5E28;
+  }
+</style>
