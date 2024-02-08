ad70f7aa8af8a21eeff92f8788b0493abdf12d92
Mads Balslev <madspbalslev@gmail.com>
2024-02-08 14:27:56 +0100

    Add small message

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,3 +8,18 @@
 {#each projects as project}
   <ProjectCard {project} />
 {/each}
+
+<p class="message">You can also check out all of my unfinished projects on my <a href="www.github.com/madsbalslev" target="_blank">GitHub</a></p>
+
+<style>
+  .message {
+    margin-top: 2rem;
+    text-align: center;
+  }
+
+  .message a {
+    color: #eb5e28;
+    text-decoration: underline;
+    font-weight: bold;
+  }
+</style>
